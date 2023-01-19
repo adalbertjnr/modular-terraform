2cb1d146ccfa0e95ebacc2b77d94d6a271ded22a
Adalberto Souza <ubuntu@ip-172-31-38-64.ec2.internal>
2023-01-19 19:18:17 +0000

    Added lb target group

--- loadbalancing/main.tf
+++ loadbalancing/main.tf
@@ -6,3 +6,16 @@ resource "aws_lb" "lb" {
     security_groups = [var.security_groups]
     idle_timeout = 400
 }
+
+resource "aws_lb_target_group" "tg" {
+    name = "tg-lb-${substr(uuid(), 0, 3)}"
+    port = var.tg_port
+    protocol = var.tg_protocol
+    vpc_id = var.vpc_id
+    health_check {
+        healthy_threshold = var.lb_healthy_threshold
+        unhealthy_threshold = var.lb_unhealthy_threshold
+        timeout = var.lb_timeout
+        interval = var.lb_interval
+    }
+}
