4003d25a776b964c6f9b1b89c8efddfa1f4be2a1
Adalberto Souza <ubuntu@ip-172-31-38-64.ec2.internal>
2023-01-19 19:18:51 +0000

    Lb target group variables

--- loadbalancing/variables.tf
+++ loadbalancing/variables.tf
@@ -3,3 +3,17 @@
 variable "pub_subnetcidr" {}
 
 variable "security_groups" {}
+
+variable "tg_port" {}
+
+variable "tg_protocol" {}
+
+variable "lb_healthy_threshold" {}
+
+variable "lb_unhealthy_threshold" {}
+
+variable "lb_timeout" {}
+
+variable "lb_interval" {}
+
+variable "vpc_id" {}
