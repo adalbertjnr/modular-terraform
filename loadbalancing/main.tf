ce9bd444d69ce2287ad585e4f7894c7f897325a7
Adalberto Souza <ubuntu@ip-172-31-38-64.ec2.internal>
2023-01-20 21:04:06 +0000

    Fixed typo in type

--- loadbalancing/main.tf
+++ loadbalancing/main.tf
@@ -29,7 +29,7 @@ resource "aws_lb_listener" "listener" {
     port = var.listener_port
     protocol = var.listener_protocol
     default_action {
-        type = "foward"
+        type = "forward"
         target_group_arn = aws_lb_target_group.tg.arn
     }
 }
