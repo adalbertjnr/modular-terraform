2d764eaaaeb147c359de01a8b01b37f6a41f9f25
Adalberto Souza <ubuntu@ip-172-31-38-64.ec2.internal>
2023-01-20 21:02:23 +0000

    Updated compute variables file with new vars

--- compute/variables.tf
+++ compute/variables.tf
@@ -9,3 +9,19 @@ variable "public_subnet" {}
 variable "public_security" {}
 
 variable "volume_size" {}
+
+variable "key_name" {}
+
+variable "public_key_path" {}
+
+variable "db_username" {}
+
+variable "db_password" {}
+
+variable "db_name" {}
+
+variable "db_endpoint" {}
+
+variable "user_data_path" {}
+
+variable "lb_target_group_arn" {}
