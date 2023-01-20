db2ed5d906b32a220ea27a56c0d1a0384047ea9c
Adalberto Souza <ubuntu@ip-172-31-38-64.ec2.internal>
2023-01-20 23:36:00 +0000

    Outputs for name, ip and port

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
+# -------------------------- MAIN/outputs.tf ---------------------------#
+
 output "lb_dns_out_root" {
     value = module.loadbalancing.lb_dns
+}
+
+
+output "root_instances_info" {
+    value = [for i in module.compute.instance_info : join(" --> ", [i.tags.Name], [join(":", [i.public_ip], [module.compute.instance_port])])]
+    sensitive = true
 }
